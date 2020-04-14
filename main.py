4615acca5a4ddc25c1388eb7c679283929f3fb1c
Mohamed <elmoufid.mohamed@outlook.com>
2020-04-15 00:30:39 +0200

    Add Basic Scanner

---
+++ main.py
@@ -0,0 +1,21 @@
+import socket
+
+target = "127.0.0.1"
+
+
+def scanport(port):
+    global target
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        return True
+    except:
+        return False
+
+
+for port in range(1, 1024):
+    result = scanport(port)
+    if (result):
+        print("Port {} is open".format(port))
+    else:
+        print("Port {} is closed".format(port))
