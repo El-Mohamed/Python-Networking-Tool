582150541e3d8b63e61506172061381294184600
Mohamed <elmoufid.mohamed@outlook.com>
2020-04-15 00:55:16 +0200

    Add Scroller To View Result (thread)

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import socket
 from tkinter import *
+from tkinter import scrolledtext
+import threading
 
 
 target = "127.0.0.1"
+
+
+window = Tk()
+window.title("MM Network Tool")
+window.geometry('600x600')
+
+scroller = scrolledtext.ScrolledText(window, width=40, height=10)
+scroller.grid(column=0, row=0)
 
 
 def scanport(port):
@@ -16,22 +26,29 @@ def scanport(port):
 
 
 def startScanning():
+    global scroller
+    message = None
     for port in range(1, 1024):
         result = scanport(port)
         if (result):
-            print("Port {} is open".format(port))
+            message = "Port {} is open".format(port)
+
         else:
-            print("Port {} is closed".format(port))
+            message = "Port {} is closed".format(port)
+
+        message += "\n"
+
+        print(message)
+        scroller.insert(INSERT, message)
 
 
-window = Tk()
+def click():
+    x = threading.Thread(target=startScanning, args=())
+    x.start()
 
-window.title("MM Network Tool")
 
-window.geometry('600x600')
-
-startButton = Button(window, text="Start Scanning", command=startScanning)
-
+startButton = Button(window, text="Start Scanning", command=click)
 startButton.grid(column=1, row=0)
+
 
 window.mainloop()
